e792653f3cf8360352e071c3e1c25d374a917a3e
Cadulox <engenharia.lourenco@gmail.com>
2021-02-26 22:05:32 -0300

    Projeto criado

---
+++ src/test/java/com/cadulox/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadulox.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
