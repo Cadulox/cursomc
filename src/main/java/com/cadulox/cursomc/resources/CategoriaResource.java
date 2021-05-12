e8253bae94d4620004d95453c4a6bf9a662fb43f
Cadulox <engenharia.lourenco@gmail.com>
2021-05-12 20:28:10 -0300

    Criando repository e service para Categoria

--- src/main/java/com/cadulox/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cadulox/cursomc/resources/CategoriaResource.java
@@ -1,28 +1,26 @@
 package com.cadulox.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cadulox.cursomc.domain.Categoria;
+import com.cadulox.cursomc.services.CategoriaService;
 
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
 
-	@GetMapping
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1L, "Informática");
-		Categoria cat2 = new Categoria(2L, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	@GetMapping("{id}")
+	public ResponseEntity<Categoria> find(@PathVariable Long id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
