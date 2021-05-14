20b667b5a8ed018988243de301978c21cd943923
Cadulox <engenharia.lourenco@gmail.com>
2021-05-13 22:48:30 -0300

    Estado e cidade

---
+++ src/main/java/com/cadulox/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.cadulox.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cadulox.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Long> {
+
+}
