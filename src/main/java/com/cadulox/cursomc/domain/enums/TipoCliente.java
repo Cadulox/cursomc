35dabbe2c625f3667f2aa99cca138b6c0892ea43
Cadulox <engenharia.lourenco@gmail.com>
2021-05-15 10:21:31 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/cadulox/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,37 @@
+package com.cadulox.cursomc.domain.enums;
+
+public enum TipoCliente {
+
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+	
+	private int cod;
+	private String descricao;
+	
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static TipoCliente toEnum(Integer cod) {
+		if (cod == null) {
+			return null;
+		}
+		
+		for (TipoCliente value : TipoCliente.values()) {
+			if (cod.equals(value.getCod())) {
+				return value;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+}
