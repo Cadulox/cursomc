5088771c2137c1cc6a194c1e5afb770cf89f029c
Cadulox <engenharia.lourenco@gmail.com>
2021-05-15 19:13:23 -0300

    Listando todas categorias com DTO

--- src/main/java/com/cadulox/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cadulox/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.cadulox.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cadulox.cursomc.domain.Categoria;
+import com.cadulox.cursomc.dtos.CategoriaDTO;
 import com.cadulox.cursomc.services.CategoriaService;
 
 
@@ -53,5 +56,12 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
