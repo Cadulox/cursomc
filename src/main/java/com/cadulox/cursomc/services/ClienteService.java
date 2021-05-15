b31e7e7b5da7a26a70f472709aed2464a74bbbb6
Cadulox <engenharia.lourenco@gmail.com>
2021-05-15 10:32:08 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/cadulox/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.cadulox.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cadulox.cursomc.domain.Cliente;
+import com.cadulox.cursomc.repositories.ClienteRepository;
+import com.cadulox.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Long id) {
+		Optional<Cliente> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
