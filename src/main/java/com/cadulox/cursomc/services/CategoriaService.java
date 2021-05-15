8e478dd5e75231a52c3ada8ded09023aea22feac
Cadulox <engenharia.lourenco@gmail.com>
2021-05-15 19:01:45 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/cadulox/cursomc/services/CategoriaService.java
+++ src/main/java/com/cadulox/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.cadulox.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.cadulox.cursomc.domain.Categoria;
 import com.cadulox.cursomc.repositories.CategoriaRepository;
+import com.cadulox.cursomc.services.exceptions.DataIntegrityException;
 import com.cadulox.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Long id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excliur uma categoria que possui produtos!");
+		}
 	}
 }
