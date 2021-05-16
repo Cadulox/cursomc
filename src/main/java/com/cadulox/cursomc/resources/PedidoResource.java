bb8e242e37e296cc6eeb4ba6fd79068d17b9f30a
Cadulox <engenharia.lourenco@gmail.com>
2021-05-16 18:45:15 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/cadulox/cursomc/resources/PedidoResource.java
+++ src/main/java/com/cadulox/cursomc/resources/PedidoResource.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.cadulox.cursomc.domain.Categoria;
 import com.cadulox.cursomc.domain.Pedido;
-import com.cadulox.cursomc.dtos.CategoriaDTO;
 import com.cadulox.cursomc.services.PedidoService;
 
 
