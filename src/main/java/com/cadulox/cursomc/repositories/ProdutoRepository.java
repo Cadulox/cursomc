f147b6998da14345e65f6d6ac25702fc64bb2d71
Cadulox <engenharia.lourenco@gmail.com>
2021-05-16 16:59:36 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/cadulox/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/cadulox/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,25 @@
 package com.cadulox.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cadulox.cursomc.domain.Categoria;
 import com.cadulox.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
+	/*@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);*/
+	
+	// Usando o padrão de nomes do Spring não é necessário a @Query do JPQL, se a @Query for mantida ela sobrepõe o padrão do Spring
+	@Transactional(readOnly = true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto>  findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
